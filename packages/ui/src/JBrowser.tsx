a7a4dcd3495e738d23e73a6e760fda79daf564bb
LiDingyi <10294099+li-tincture-instrument@user.noreply.gitee.com>
2026-04-03 11:32:16 +0800

    显示基因浏览器

--- packages/ui/src/JBrowser.tsx
+++ packages/ui/src/JBrowser.tsx
@@ -3,53 +3,50 @@ import { JBrowseApp,createViewState } from '@jbrowse/react-app2';
 
 const viewState = createViewState({
     config: {
-        assemblies: {
-            name: 'hg38',
-            sequence: {
-                type: 'ReferenceSequenceTrack',
-                trackId: 'GRCh38-ReferenceSequenceTrack',
-                adapter: {
-                    type: 'BgzipFastaAdapter',
-                    uri: 'https://jbrowse.org/genomes/GRCh38/fasta/hg38.prefix.fa.gz',
-                },
-            },
-            refNameAliases: {
-                adapter: {
-                    type: 'RefNameAliasAdapter',
-                    uri: 'https://jbrowse.org/genomes/GRCh38/hg38_aliases.txt',
-                },
-            },
-            cytobands: {
-                adapter: {
-                    type: 'CytobandAdapter',
-                    uri: 'https://jbrowse.org/genomes/GRCh38/cytoBand.txt',
-                },
-            },
-        },
-        tracks: [
+        assemblies: [
             {
-                type: 'FeatureTrack',
-                trackId: 'ncbi_genes',
-                name: 'NCBI RefSeq Genes',
-                assemblyNames: ['hg38'],
-                adapter: {
-                    type: 'Gff3TabixAdapter',
-                    uri: 'https://jbrowse.org/genomes/GRCh38/ncbi_refseq/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz',
+                name: 'hg38',
+                sequence: {
+                    trackId: 'GRCh38-ReferenceSequenceTrack',
+                    type: 'ReferenceSequenceTrack',
+                    adapter: {
+                        type: 'BgzipFastaAdapter',
+                        fastaLocation: {
+                            uri: 'https://jbrowse.org/genomes/GRCh38/fasta/hg38.prefix.fa.gz',
+                        },
+                        faiLocation: {
+                            uri: 'https://jbrowse.org/genomes/GRCh38/fasta/hg38.prefix.fa.gz.fai',
+                        },
+                    },
+                },
+                refNameAliases: {
+                    adapter: {
+                        type: 'RefNameAliasAdapter',
+                        uri: 'https://jbrowse.org/genomes/GRCh38/hg38_aliases.txt',
+                    },
+                },
+                cytobands: {
+                    adapter: {
+                        type: 'CytobandAdapter',
+                        uri: 'https://jbrowse.org/genomes/GRCh38/cytoBand.txt',
+                    },
                 },
             },
         ],
-        defaultSession: {
-            name: 'My session',
-            view: {
-                id: 'linearGenomeView',
-                type: 'LinearGenomeView',
-                init: {
-                    assembly: 'hg38',
-                    loc: '10:29,838,565..29,838,850',
-                    tracks: ['ncbi_genes'],
+        tracks: [
+            {
+                trackId: 'ncbi_genes',
+                name: 'NCBI RefSeq Genes',
+                type: 'FeatureTrack',
+                assemblyNames: ['hg38'],
+                adapter: {
+                    type: 'Gff3TabixAdapter',
+                    gffGzLocation: {
+                        uri: 'https://jbrowse.org/genomes/GRCh38/ncbi_refseq/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz',
+                    },
                 },
             },
-        },
+        ],
     }
 });
 
